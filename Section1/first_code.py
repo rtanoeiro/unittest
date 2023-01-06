789f39f876357aa1651273eb780b50cc5e4dba0c
Ramon Tanoeiro <ramontanoeiro@gmail.com>
2023-01-06 13:22:40 +1100

    Added first_code and first_test_code python files

---
+++ Section1/first_code.py
@@ -0,0 +1,32 @@
+"""
+Section 1 first file
+"""
+
+
+def upper_name(name):
+    """
+    This function returnes the given name in upper characters
+    input: str
+    output: upper str
+    """
+    return name.upper()
+
+
+if upper_name("Ramon") == "RAMON":
+    print("Success!")
+else:
+    print("Fail!")
+
+def avg(list_numbers):
+    """
+    This function returnes the average of all given numbers
+    input: str
+    output: upper str
+    """
+    total = 0
+    for num in list_numbers:
+        total += num
+    return total / len(list_numbers)
+
+
+print(avg([1, 2, 3]))
