94ef76ed3abc3cb20465418c5b761187385e8ea2
Ramon Tanoeiro <ramontanoeiro@gmail.com>
2023-01-06 12:44:15 +1100

    Added pyenv dependencies and created first python code

---
+++ Section1/testing.py
@@ -0,0 +1,18 @@
+'''
+Section 1 first file
+'''
+
+def upper_name(name):
+    '''
+    This function returnes the given name in upper characters
+    input: str
+    output: upper str
+    '''
+    return name.upper()
+
+if upper_name("Ramon")=="RAMON":
+    print("Success!")
+else:
+    print("Fail!")
+
+#print(upper_name("Ramon"))
