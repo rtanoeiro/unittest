fb426e2f6f8a95fe6932ed8b20cca808b5d30a26
rtanoeiro <ramontanoeiro@gmail.com>
2023-02-13 13:53:34 +1100

    Updated last challenge, but it didn't make no sense

---
+++ sections/section21/term.py
@@ -0,0 +1,11 @@
+class Term:
+    def __init__(self, coefficient, power):
+        self.coefficient = coefficient
+        self.power = power
+        pass
+
+    def get_power(self):
+        return self.power
+
+    def get_coefficient(self):
+        return self.coefficient
